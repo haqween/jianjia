0aef615ba347905d151440375471fba7574ce141
haqween <haqween@163.com>
2020-02-09 13:47:10 +0800

    init webshell

---
+++ webshell/src/main/java/hq/jianjia/App.java
@@ -0,0 +1,13 @@
+package hq.jianjia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
